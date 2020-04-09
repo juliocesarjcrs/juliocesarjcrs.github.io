bc4026ef54240e2327fc34fcd0abe8bded9b46f4
julio <julio.rico@bluewebfactory.com.co>
2020-04-09 17:15:43 -0500

    change in page curriculum

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 let link = document.querySelector('.navbar-nav')
+
 link.addEventListener('click', activarlink)
 
 function activarlink(evt){
@@ -9,4 +10,20 @@ function activarlink(evt){
         allLinks[i].classList.remove('active')
     }
     wantActive.classList.add('active')  
+}
+
+function continuarLeyendo(evt){
+    evt.preventDefault()
+    console.log('continuarLeyendo', evt.target.parentNode.parentNode.children);
+    if(evt.target.parentNode.children[3].classList.contains('biography')){
+        evt.target.parentNode.children[3].classList.remove('biography')
+        evt.srcElement.innerHTML = 'Reducir'
+        evt.target.parentNode.parentNode.children[1].classList.remove('d-none')
+    }else{
+        evt.target.parentNode.children[3].classList.add('biography')
+        evt.srcElement.innerHTML = 'Continuar leyendo'
+        evt.target.parentNode.parentNode.children[1].classList.add('d-none')
+    }
+
+    
 }
